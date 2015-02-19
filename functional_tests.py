26574017ccca1724226359cec3e09c1c49ac6ec4
rumbodeveloper <rumbo.developer@rumbo181.com>
2015-02-19 21:42:22 +0100

    commit inicial: cambios en settings y despliegue del proyecto

---
+++ functional_tests.py
@@ -0,0 +1,19 @@
+__author__ = 'Angel_Luis'
+
+'''
+Date = '8/2/15'
+'''
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+
+assert ('Main Page'in browser.title, True)
+browser.implicitly_wait(3)
+time.sleep(5)
+browser.quit()
+
