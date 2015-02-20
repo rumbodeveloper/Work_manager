901e9464118600afaade848558d61bfeb1e1fec4
rumbodeveloper <rumbo.developer@rumbo181.com>
2015-02-20 22:31:18 +0100

    agregado un primer ejemplo de templates y static files

--- TaskManager/views.py
+++ TaskManager/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'en/public/index.html')
+
+def connection(request):
+    return render(request, 'en/public/connection.html')
--- Work_manager/urls.py
+++ Work_manager/urls.py
@@ -8,11 +8,13 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Work_manager.views.home', name='home'),
     # url(r'^Work_manager/', include('Work_manager.foo.urls')),
-    url(r'^$','TaskManager.views.index'),
-    url(r'^index$','TaskManager.views.index'),
+    url(r'^$','TaskManager.views.index',name='public_index'),
+    url(r'^index$','TaskManager.views.index',name='public_index'),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^connection$','TaskManager.views.connection',name='public_connection'),
 )
