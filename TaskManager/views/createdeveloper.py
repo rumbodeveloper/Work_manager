acb66cfef5c0a826c7b26654416fe8daa0e21b4c
rumbodeveloper <rumbo.developer@rumbo181.com>
2015-02-22 16:10:29 +0100

    agregado view para agregar desarrollador al modelo

--- TaskManager/views/createdeveloper.py
+++ TaskManager/views/createdeveloper.py
@@ -1,11 +1,47 @@
 
 # Create your views here.
 from django.shortcuts import render
+from django.http import HttpResponse
 
-from TaskManager.models import   Supervisor
+from TaskManager.models import  Developer, Supervisor
 
 
+
+'''def page(request):
+    supervisors_list = Supervisor.objects.all()
+    return render(request, 'en/public/create_developer.html',{'supervisors_list': supervisors_list})'''
 
 def page(request):
-    supervisors_list = Supervisor.objects.all()
-    return render(request, 'en/public/create_developer.html',{'supervisors_list': supervisors_list})
+    error = False
+    
+    if request.POST:
+        if 'name' in request.POST:
+            name = request.POST.get('name','')
+        else:
+            error=True
+
+        if 'login' in request.POST:
+            login = request.POST.get('login','')
+        else:
+            error=True
+            
+        if 'password' in request.POST:
+            password = request.POST.get('password','')
+        else:
+            error=True
+            
+        if 'supervisor' in request.POST:
+            supervisor_id = request.POST.get('supervisor','')
+        else:
+            error=True
+            
+        if not error:
+            supervisor = Supervisor.objects.get(id=supervisor_id)
+            new_dev = Developer(name=name, login=login, password=password, supervisor=supervisor)
+            new_dev.save()
+            return HttpResponse('Desarrollador agregado')
+        else:
+            return HttpResponse('Ha habido algun error')
+    else:
+        supervisors_list = Supervisor.objects.all()
+        return render(request, 'en/public/create_developer.html',{'supervisors_list': supervisors_list})
